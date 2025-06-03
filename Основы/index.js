f5973c1a2a626939420fdc3de89657233139c779
tenggys <levgood21@yandex.ru>
2025-06-03 18:23:18 +0500

    JS практика

--- Основы/index.js
+++ Основы/index.js
@@ -260,4 +260,378 @@ else {
 }
 */
 
+/*
+Создать переменные num1, num2 которые пользователь вводит с клавиатуры
+Проверьте, что переменная num1 равна или меньше 1, а переменная num2 больше или равна 3.
+*/
+/*
+const num1 = Number(prompt('первое число'));
+const num2 = Number(prompt('второе число'));
 
+if (num1 === 1) {
+    console.log(`${num1} равно 1`);
+} else if (num1 < 1) {
+    console.log(`${num1} меньше 1`);
+}
+
+if (num2 === 3) {
+    console.log(`${num2} равно 3`);
+} else if (num2 > 3) {
+    console.log(`${num2} больше 3`);
+}
+*/
+/*Перепишите код к тернарному оператору
+
+let test = true;
+if (test === true) {
+console.log('+++');
+} else {
+console.log('---');
+}
+*/
+let test = true;
+console.log((test === true) ? '+++' : '---');
+
+/*
+В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+*/
+const day = 12;
+
+if (day > 0 && day <= 9 ) {
+    console.log(`${day} попадает в первую декаду месяца`)
+} else if (day > 9 && day <= 20 ) {
+    console.log(`${day} попадает в вторую декаду месяца`)
+} else if (day > 20 && day <= 31 ) {
+    console.log(`${day} попадает в втретью декаду месяца`)
+}
+/*
+Необходимо от пользователя получить число.
+Необходимо вывести разряды числа, а именно: количество сотен, десятков и единиц.
+*/
+/*
+const isNum = Number(prompt('Ведите число'));
+
+const hundreds = Math.trunc(isNum%1000/100);
+const dozens = Math.trunc(isNum%100/10);
+const units = Math.trunc(isNum%10);
+
+alert(`В числе ${isNum} количество сотен: ${hundreds}, десятков: ${dozens}, единиц: ${units}`);
+*/
+
+//ФУНКЦИИ
+/*
+const ivanov = 4.5;
+const petrov = 3.7;
+const sidorov = 3.2;
+
+let scholarshipIvanov = 0;
+if (ivanov > 3.4 && ivanov < 4) {
+    scholarshipIvanov = 5000;
+} else if (ivanov > 4) {
+    scholarshipIvanov = 7500;
+}
+
+let scholarshipPetrov = 0;
+if (petrov > 3.4 && petrov < 4) {
+    scholarshipPetrov = 5000;
+} else if (petrov > 4) {
+    scholarshipPetrov = 7500;
+}
+
+let scholarshipSidarov = 0;
+if (sidorov > 3.4 && sidorov < 4) {
+    scholarshipSidarov = 5000;
+} else if (sidorov > 4) {
+    scholarshipSidarov = 7500;
+}
+
+console.log(scholarshipIvanov);
+console.log(scholarshipPetrov);
+console.log(scholarshipSidarov);
+*/
+//реализация вышеприведенного примера с помощью функции
+
+function getScholarship(academicPerformance) {
+    if (academicPerformance < 3.4) {
+        return 0;
+    }
+    if (academicPerformance < 4) {
+        return 5000;
+    } else {
+        return 7500;
+    }
+}
+
+const ivanov = 4.5;
+const petrov = 3.7;
+const sidorov = 3.2;
+
+let scholarshipIvanov = getScholarship(ivanov);
+let scholarshipPetrov = getScholarship(petrov);
+let scholarshipSidarov = getScholarship(sidorov);
+
+console.log(scholarshipIvanov);
+console.log(scholarshipPetrov);
+console.log(scholarshipSidarov);
+
+const sum1 = function(a, b) {
+return a + b;
+}
+// Код с использованием стрелочной функциии
+const f = (a,b) => a + b;
+
+function goRiddle() {
+    function riddles(question, answer) {
+        let userAnswer = prompt(question);
+        userAnswer = userAnswer.toLocaleLowerCase();
+        if (userAnswer === answer) {
+            alert('верно')
+        } else {
+            alert('не верно');
+        }
+    }
+    riddles('Не лает, не кусает, а в дом не пускает', 'замок');
+    riddles('Сто одёжек и все без застежек', 'капуста');
+}
+/*
+// Плохие имена функций.
+const user = nameOfUser(); // Понятно, что функция что-то делает с именем
+пользователя. Можно догадаться, что получает его имя, но для полного понимания
+надо заходить в функцию.
+const hourlyRate = rateTable(user); // Тут непонятно, что делает функция.
+Может, она устанавливает какой-то рейт для пользователя или возвращает его
+рейт.
+const workedHours = tableOfhours(user); // То же самое, что и выше.
+const result = salary(hourlyRate, workedHours); // Ну и, как результат, мы
+получаем зарплату пользователя, а может быть, сравниваем внутри что-то.
+// Ещё один пример плохого имени, когда функция делает больше, чем положено.
+Const access = getUserByNameSaveDBAuth(user);
+// Хорошие имена функций — код читается как книга и всё понятно.
+const user = getUserFromDB();
+const hourlyRate = getUserHourlyRate(user);
+const workedHours = getUserWorkedHours(user);
+const result = calculateUserSallary(hourlyRate, workedHours);
+// Тут мы видим, что получаем пользователя из базы данных, потом получаем его
+часовую ставку и количество отработанных этим пользователем часов и в итоге
+рассчитываем его зарплату.
+*/
+
+/*Написать функцию, которой передаем, имя, фамилия и
+возраст, и получаем строку "Привет Иван Петров с
+возрастом 17" (только здесь данные, которые были
+переданы в функцию)
+2. Создайте функцию которая возводит переданное число в
+квадрат
+3. Сделайте функцию, которая параметром принимает
+число и проверяет, положительное это число или
+отрицательное. В первом случае пусть функция выводит
+в консоль текст '+++', а во втором '---'.
+*/
+
+function user(name, lastName, age) {
+    return `Привет ${name} ${lastName} с возростом ${age} лет`;
+}
+console.log(user('Yura', 'Gulyaev', 31));
+
+function squareNumber(number) {
+    return number * number;
+}
+console.log(squareNumber(6));
+
+function checkNumber(number) {
+    if (number > 0) {
+        return '+++'
+    } else if (number < 0) {
+        return '---'
+    }
+}
+console.log(checkNumber(-5));
+
+/*
+Сделайте функцию, которая параметрами принимает 3 числа и
+выводит в консоль сумму этих чисел.
+2. С помощью созданной вами функции выведите в консоль сумму
+значений этих переменных.
+let param1 = 1;
+let param2 = 2;
+let param3 = 3;
+1. Дана фукнция
+function func(num = 5) {
+console.log(num * num);
+}
+Расскажите, каким будет результат каждого из вызовов функции.
+func(2);
+func(3);
+func();
+*/
+
+const param1 = 1;
+const param2 = 2;
+const param3 = 3;
+
+function sumTreeMumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+console.log(sumTreeMumbers(param1, param2, param3));
+
+function func(num = 5) {
+console.log(num * num);
+}
+
+func(2); //4
+func(3); //9
+func(); //25
+
+/*Сделайте функцию, которая параметром принимает число, а
+возвращает квадратный корень из этого числа. С помощью этой
+функции найдите корень числа 3, затем найдите корень числа 4.
+Просуммируйте полученные результаты и выведите их в консоль.
+2. Создайте функцию, которая находит минимальное число из 2х
+передаваемых аргументов функции
+*/
+function squareRoot(num) {
+    return Math.sqrt(num)
+}
+
+const a = squareRoot(4);
+const b = squareRoot(9);
+const sum = a + b;
+console.log(sum);
+
+function findMin(num1, num2) {
+    return Math.min(num1, num2)
+}
+console.log(findMin(a, b));
+
+/*Сделайте функцию, которая принимает параметром число от 1 до 7,
+а возвращает день недели на русском языке.
+2. Написать функцию, которой передаем имя и она возвращает
+приветствие в зависимости от времени суток (Доброе
+утро\день\вечер\ночи Иван)*/
+
+function getDay(day) {
+    switch (day) {
+        case 1:
+            return 'Понедельник';
+            break;
+        case 2:
+            return 'Вторник';
+            break;
+        case 3:
+            return 'Среда';
+            break;
+        case 4:
+            return 'Четверг';
+            break;
+        case 5:
+            return 'Пятница';
+            break;
+        case 6:
+            return 'Суббота';
+            break;
+        case 7:
+            return 'Воскресенье';
+            break;
+        default: 
+        return 'Не верное число';
+    }
+}
+console.log(getDay(7));
+
+function greet(name) {
+    const now = new Date();
+    const hour = now.getHours();
+
+    if (hour >= 6 && hour < 12) {
+        return `доброе утро ${name}`;
+    } else if (hour >= 12 && hour < 18) {
+        return `добрый день ${name}`;
+    } else if (hour >= 18 && hour < 24) {
+        return `добрый вечер ${name}`;
+    } else {
+        return `доброй ночи ${name}`;
+    }
+}
+console.log(greet('Юра'));
+
+/*Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+*/
+function cubeNumber(num) {
+    return num * num * num;
+}
+const q = cubeNumber(2);
+const w = cubeNumber(3);
+const sumQw = q + w;
+console.log(sumQw);
+
+/*Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
+Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
+*/
+/*
+function inputSomeValue() {
+    const userAnswer = prompt('Введите число');
+
+    if (isNaN(userAnswer)) {
+        return alert('Значение задано не верно');
+    } else {
+        return alert('Правильно! Вы ввели число!');
+    }
+}
+console.log(inputSomeValue());
+*/
+const cache = 10000;
+const tax = cache * 0.13;
+const salary = cache - tax;
+
+function procentMony(mony) {
+    return `Размер заработной платы за вычетом налогов равен ${mony}`;
+}
+
+console.log(procentMony(salary));
+
+/*Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
+*/
+/*
+const numberOne = Number(prompt('Введите первое число'));
+const numberTwo = Number (prompt('Введите второе число'));
+const numberFree = Number (prompt('Введите третье число'));
+
+function getNumberMax(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+}
+console.log(getNumberMax(numberOne, numberTwo, numberFree));
+*/
+/*Необходимо реализовать четыре функции, каждая функция должна принимать по два числа и выполнять одну из операций (каждая функция выполняет одну из них):
+1. Сложение
+2. Разность
+3. Умножение
+4. Деление
+Необходимо сделать так, чтобы функция вернула число, например выражение console.log(sum(2, 6)); должно вывести число 8 в консоль (sum - функция сложения в данном примере, ваши названия функций могут отличаться). Округлять значения, которые возвращают функции не нужно, однако, обратите внимание на разность, функция должна вычесть из большего числа меньшее, либо вернуть 0, если числа равны. Функциям всегда передаются корректные числа, проверки на NaN, Infinity делать не нужно.
+*/
+
+function sum2(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    if (a > b) {
+        return a - b;
+    } else {
+        return b - a;
+    }
+}
+
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+console.log(sum2(2, 6));
+console.log(sub(2, 3));
+console.log(mult(8, 7));
+console.log(div(5, 7));
+
+//Циклы и массивы
